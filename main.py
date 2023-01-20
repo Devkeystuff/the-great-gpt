2f11bf50d773dc75167892d1e27e0afbde6f2958
smudd <83008886+smudd@users.noreply.github.com>
2023-01-20 14:56:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ PROMPTS = [
 
 def ask_questions(chapter: int):
     #chapter that we will make chatgpt read
-    chptr = ""
+    chapter_pieces = []
 
     #fetch raw chapter text
     with open('../great_expectation.htm', 'r') as f:
@@ -29,16 +29,18 @@ def ask_questions(chapter: int):
         soup = BeautifulSoup(contents, 'html.parser')
 
         #find chapter
-        chap = soup.find("a", {"name": "chap{0}".format(str(chapter).zfill(2))})
+        chap = soup.find("a", {"name": "chap{0}".format(str('2').zfill(2))})
+        chap_text = chap.parent.parent.get_text()
 
-        #get text of chapter parent parent element that holds <p> elements with chapter text
-        chptr = """ here: "{0}" can I ask you some questions about this chapter? """.format(chap.parent.parent.get_text())
+        for part in split_by_number_of_words(chap_text, 1998):
+            chapter_pieces.append(part)
 
-    response = bot.ask(chptr)
-    print(response)
+    for chapter_piece in chapter_pieces:
+        rsp = bot.ask("""here's a part of chapter {0}, : "{1}" """.format(chapter, chapter_pieces))
+        print(rsp)
     
     for prompt in PROMPTS:
-        prompt = prompt.format("this chapter")
+        prompt = prompt.format("chapter {}".format(chapter))
         if os.path.exists(f"{chapter}.txt"):
             with open(f"{chapter}.txt", "r") as file:
                 prompt_exists = False
@@ -54,6 +56,9 @@ def ask_questions(chapter: int):
             save_answers(chapter, prompt, response)
         else:
             print(f"Failed to find an answer to: {prompt}")
+    
+    rsp = bot.ask("That's all for this chapter, can I give you another one?")
+    print(rsp)
 
 def save_answers(chapter: str, question: str, answer: str, mode = "w"):
     if os.path.exists(f"{chapter}.txt"):
@@ -62,6 +67,9 @@ def save_answers(chapter: str, question: str, answer: str, mode = "w"):
         file.write(question + "\n")
         file.write(answer + "\n\n")
 
+def split_by_number_of_words(input, number_of_words):
+    regexp = re.compile(r'((?:\w+\W+){0,%d}\w+)' % (number_of_words - 1))
+    return regexp.findall(input)
 
 if __name__ == "__main__":
     bot = ChatGPT()
